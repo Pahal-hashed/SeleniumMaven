9e905246e4954c3d2b8aa4cee6a953c827774c57
Pahal-hashed <pahaldhavale2017@gmail.com>
2020-11-07 14:25:54 -0800

    Added Exe

--- src/test/java/TestNGTestCase/AddBook.java
+++ src/test/java/TestNGTestCase/AddBook.java
@@ -13,7 +13,7 @@ public class AddBook {
 	WebDriver driver;
 	@BeforeMethod
 	public void launch() {
-		System.setProperty("webdriver.gecko.driver","/home/pahal/Selenium_JAR/geckodriver");
+		System.setProperty("webdriver.gecko.driver","geckodriver");
 		  driver =new FirefoxDriver();
 		  driver.get("https://www.google.com");
 		  driver.manage().window().maximize();
