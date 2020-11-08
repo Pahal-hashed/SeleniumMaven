40754dcd6bc7b6e2f6ab67725606bb6cb2969eb5
Pahal-hashed <pahaldhavale2017@gmail.com>
2020-11-08 08:39:42 -0800

    NewCommitAssignment

--- src/test/java/TestNGTestCase/AddBook.java
+++ src/test/java/TestNGTestCase/AddBook.java
@@ -13,20 +13,20 @@ public class AddBook {
 	WebDriver driver;
 	@BeforeMethod
 	public void launch() {
-		System.setProperty("webdriver.gecko.driver","geckodriver");
+		System.setProperty("webdriver.gecko.driver","/home/pahal/Selenium_JAR/geckodriver");
 		  driver =new FirefoxDriver();
-		  driver.get("https://www.google.com");
+		  driver.get("http://0.0.0.0:3001/");
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}		
   @Test
   public void Verifyaddbook() {
-		driver.findElement(By.xpath("//a[@href='https://www.google.co.in/imghp?hl=en&tab=wi&ogbl']")).click();
-		driver.findElement(By.name("q")).sendKeys("Gracias");
-		driver.findElement(By.xpath("//div[@class='FAuhyb']")).click();
+	  	driver.findElement(By.name("login")).sendKeys("devops");
+	  	driver.findElement(By.name("password")).sendKeys("test");
+	  	driver.findElement(By.name("click")).click();		
   }
 	@AfterMethod
 	public void close() {
-		driver.close();
+//		driver.close();
 	}
 }
